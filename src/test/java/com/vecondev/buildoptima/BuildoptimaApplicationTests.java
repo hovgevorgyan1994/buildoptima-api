ae837631dc2ee4a250a4714fe3c9fdbc9b81d4aa
Gooner9803 <vache,khachatryan1998@gmail.com>
2022-06-07 19:14:06 +0400

    [BO-13] Created application skeleton

---
+++ src/test/java/com/vecondev/buildoptima/BuildoptimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vecondev.buildoptima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildoptimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
