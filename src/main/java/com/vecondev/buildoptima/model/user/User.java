7dae0acdf889755155b78dd68b8365b85e3edd50
Hovo33651 <hovogevorgyan94@mail.ru>
2022-06-08 15:28:16 +0400

    [BO-15] Designed user database schemas

---
+++ src/main/java/com/vecondev/buildoptima/model/user/User.java
@@ -0,0 +1,50 @@
+package com.vecondev.buildoptima.model.user;
+
+import com.vecondev.buildoptima.model.Domain;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User extends Domain {
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @CreationTimestamp
+    @Column(name = "creation_date")
+    private LocalDate creationDate;
+
+    @UpdateTimestamp
+    @Column(name = "update_date")
+    private LocalDate updateDate;
+
+    @Column(name = "active")
+    private boolean active;
+}
---
+++ src/main/java/com/vecondev/buildoptima/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.vecondev.buildoptima.repository;
+
+import com.vecondev.buildoptima.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID>, PagingAndSortingRepository<User, UUID> {
+
+    boolean existsByEmailIgnoreCase(String email);
+
+    boolean existsByPhone(String phone);
+}
